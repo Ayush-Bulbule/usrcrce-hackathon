21dfe023517b1bc819532a33363f1848c9b26acb
mayuri kolhe <kolhem10@gmail.com>
2023-02-10 22:47:30 +0530

    login/signup

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -1,55 +1,89 @@
-<!doctype html>
+<!DOCTYPE html>
+
+
 <html lang="en">
-
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Work Connect</title>
 
-    <title>Hello, world!</title>
+  <!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
+  <link href="/plugins/material/css/materialdesignicons.min.css" rel="stylesheet" />
+  <link href="/plugins/simplebar/simplebar.css" rel="stylesheet" />
+
+  <!-- PLUGINS CSS STYLE -->
+  <link href="/plugins/nprogress/nprogress.css" rel="stylesheet" />
+  
+  <!-- MONO CSS -->
+  <link id="main-css-href" rel="stylesheet" href="/css/style.css" />
+
+  
+
+
+  <!-- FAVICON -->
+  <link href="/images/favicon.png" rel="shortcut icon" />
+
+  <script src="/plugins/nprogress/nprogress.js"></script>
 </head>
 
-<body>
-    <h1>Hello, world!</h1>
+</head>
+  <body class="bg-light-gray" id="body">
+          <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh">
+          <div class="d-flex flex-column justify-content-between">
+            <div class="row justify-content-center">
+              <div class="col-lg-6 col-md-10">
+                <div class="card card-default mb-0">
+                  <div class="card-header pb-0">
+                    <div class="app-brand w-100 d-flex justify-content-center border-bottom-0">
+                      <a class="w-auto pl-0" href="/index.html">
+                        <img src="/images/logo.png" alt="Mono">
+                        <span class="brand-name text-dark">Work Connect</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="card-body px-5 pb-5 pt-0">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-10 mx-auto">
-                <form action="<?php echo base_url('auth/login'); ?>" method="post" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="name">Name:</label>
-                        <input type="text" class="form-control" id="name" name="name">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" class="form-control" id="email" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password:</label>
-                        <input type="password" class="form-control" id="password" name="password">
-                    </div> 
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                    <h4 class="text-dark mb-6 text-center">Sign in to Work Connect</h4>
+
+                    <form action="<?php echo base_url('auth/login'); ?>" method="post" enctype="multipart/form-data">
+                      <div class="row">
+                      <div class="form-group col-md-12 mb-4">
+                          <input type="text" class="form-control input-lg" id="name" name="name"
+                            placeholder="Name">
+                        </div>
+                        <div class="form-group col-md-12 mb-4">
+                          <input type="email" class="form-control input-lg" id="email" name="email"
+                            placeholder="Email">
+                        </div>
+                        <div class="form-group col-md-12 ">
+                          <input type="password" class="form-control input-lg" id="password" name="password" placeholder="Password">
+                        </div>
+                        <div class="col-md-12">
+
+                          <div class="d-flex justify-content-end mb-3">
+
+                            <a class="text-color" href="#"> Forgot password? </a>
+
+                          </div>
+
+                          <button type="submit" class="btn btn-primary btn-pill mb-4">Login</button>
+
+                          <p>Don't have an account yet ?
+                            <a class="text-blue" href="sign-up.html">Sign Up</a>
+                          </p>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
 
-
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
 </body>
-
 </html>
--- app/Views/Auth/register.php
+++ app/Views/Auth/register.php
@@ -1,83 +1,117 @@
-<!doctype html>
+
+
 <html lang="en">
-
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Work Connect</title>
 
-    <title>Hello, world!</title>
+  <!-- GOOGLE FONTS -->
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
+  <link href="/plugins/material/css/materialdesignicons.min.css" rel="stylesheet" />
+  <link href="/plugins/simplebar/simplebar.css" rel="stylesheet" />
+
+  <!-- PLUGINS CSS STYLE -->
+  <link href="/plugins/nprogress/nprogress.css" rel="stylesheet" />
+
+  <link id="main-css-href" rel="stylesheet" href="/css/style.css" />
+
+  
+
+
+  <!-- FAVICON -->
+  <link href="/images/favicon.png" rel="shortcut icon" />
+
+  <script src="/plugins/nprogress/nprogress.js"></script>
 </head>
 
-<body>
-    <h1>Hello, world!</h1>
+</head>
+  <body class="bg-light-gray" id="body">
+          <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh">
+          <div class="d-flex flex-column justify-content-between">
+            <div class="row justify-content-center">
+              <div class="col-md-10">
+                <div class="card card-default mb-0">
+                  <div class="card-header pb-0">
+                    <div class="app-brand w-100 d-flex justify-content-center border-bottom-0">
+                      <a class="w-auto pl-0" href="/index.html">
+                        <img src="/images/logo.png" >
+                        <span class="brand-name text-dark">Work Connect</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="card-body px-5 pb-5 pt-0">
+                    <h4 class="text-dark text-center mb-5">Worker Sign Up</h4>
+                    <form action="<?php echo base_url('auth/register'); ?>" method="post" enctype="multipart/form-data">
+                      <div class="row">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-10 mx-auto">
-                <form action="<?php echo base_url('auth/register'); ?>" method="post" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="name">Name:</label>
-                        <input type="text" class="form-control" id="name" name="name">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" class="form-control" id="email" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password:</label>
-                        <input type="password" class="form-control" id="password" name="password">
-                    </div>
-                    <div class="form-group">
-                        <label for="age">Age:</label>
-                        <input type="number" class="form-control" id="age" name="age">
-                    </div>
-                    <div class="form-group">
-                        <label for="role">Role:</label>
-                        <select class="form-control" id="role" name="role">
-                            <option value="client">Select Role</option>
-                            <option value="worker">Worker</option>
-                            <option value="client">Client</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="phone">Phone:</label>
-                        <input type="text" class="form-control" id="phone" name="phone">
-                    </div>
-                    <div class="form-group">
-                        <label for="address">Address:</label>
-                        <textarea class="form-control" id="address" name="address"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="work_cat">Work Category:</label>
-                        <input type="text" class="form-control" id="work_cat" name="work_cat">
-                    </div>
-                    <div class="form-group">
-                        <label for="idproof">ID Proof:</label>
-                        <input type="file" class="form-control-file" id="idproof" name="idproof">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+                        <div class="form-group col-md-6 mb-4">
+                          <input type="text" class="form-control input-lg" name="name" id="name" placeholder="Name">
+                        </div>
+                        <div class="form-group col-md-6 mb-4">
+                          <input type="email" class="form-control input-lg" id="email" name="email"  placeholder="Email">
+                        </div>
+                        <div class="form-group col-md-6 ">
+                          <input type="password" class="form-control input-lg" id="password" name="password" placeholder="Password">
+                        </div>
+                        <div class="form-group col-md-6 ">
+                          <input type="password" class="form-control input-lg" id="cpassword" name="" placeholder="Confirm Password">
+                        </div>
+                        <div class="form-group col-md-6 mb-4">
+                          <input type="text" class="form-control input-lg" id="work_cat" name="work_cat"  placeholder="Work Category">
+                        </div>
+                        <div class="form-group col-md-6 mb-4">
+                          <input type="number" class="form-control input-lg" id="phone" name="phone"  placeholder="Phone">
+                        </div>
+                        
+                        <div class="form-group col-md-6 mb-4">
+                          <input type="number" class="form-control input-lg" id="age" name="age" aria-describedby="agek_catHelp" placeholder="Age">
+                        </div>
+                        <div class="form-group col-md-6 mb-4">
+                          <input type="text" class="form-control input-lg" id="address" name="address" aria-describedby="address_catHelp" placeholder="Address">
+                        </div>
+
+                        <div class="form-group col-md-6 mb-4">
+                          
+                          <select class="form-control" id="role" name="role">
+                            <option>--Select Role--</option>
+                            <option>Client</option>
+                            <option>Worker</option>
+                          </select>
+                        </div>
+                        <div class="form-group col-md-6 mb-4">
+                          <label for="idproof">Upload ID Proof</label>
+                          <input type="file" class="form-control input-lg" id="idproof" name="idproof" aria-describedby="idproof" placeholder="Id Proof">
+                        </div>
+                        
+                        <div class="col-md-12">
+                          <div class="d-flex justify-content-between mb-3">
+
+                            <div class="custom-control custom-checkbox mr-3 mb-3">
+                              <input type="checkbox" class="custom-control-input" id="customCheck2">
+                              <label class="custom-control-label" for="customCheck2">I Agree the terms and conditions.</label>
+                            </div>
+
+                          </div>
+
+                          <button type="submit" class="btn btn-primary btn-pill mb-4" name="submit">Sign Up</button>
+
+                          <p>Already have an account?
+                            <a class="text-blue" href="login">Sign in</a>
+                          </p>
+                        </div>
+                      </div>
+                    </form>
+
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
 
-
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
 </body>
-
 </html>
